271f3be090b9a38a6ae3515f921fdbd57bc81b01
Lethargy2718 <176531725+Lethargy2718@users.noreply.github.com>
2025-08-14 00:27:20 +0300

    feat(flecs): add player name constant

--- flecs/src/utils/constants.hpp
+++ flecs/src/utils/constants.hpp
@@ -4,4 +4,5 @@ namespace constants {
     constexpr int WINDOW_WIDTH = 640;
     constexpr int WINDOW_HEIGHT = 640;
     constexpr int TILE_SIZE = 32;
+    constexpr const char* PLAYER_NAME = "Player";
 }
