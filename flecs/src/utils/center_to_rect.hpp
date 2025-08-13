e65b325b1d2dc9de6217f08520c494d7c1a7b944
Lethargy2718 <176531725+Lethargy2718@users.noreply.github.com>
2025-08-13 23:58:54 +0300

    feat(flecs): add utils folder
    - Added center_to_rect.hpp and constants.hpp to flecs/src/utils/
    - Moved rng.hpp to flecs/src/utils/

---
+++ flecs/src/utils/center_to_rect.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <raylib.h>
+#include "physics/components.hpp"
+
+inline Rectangle CenterToRect(const Transform2D &t, const Rectangle &r) {
+    float scaledW = r.width * t.scale.x;
+    float scaledH = r.height * t.scale.y;
+
+    return { t.position.x - scaledW/2, t.position.y - scaledH/2, scaledW, scaledH };
+}
---
+++ flecs/src/utils/constants.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+namespace Constants {
+    constexpr int WINDOW_WIDTH = 640;
+    constexpr int WINDOW_HEIGHT = 640;
+    constexpr int TILE_SIZE = 32;
+}
